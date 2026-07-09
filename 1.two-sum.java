b1ebc66df36926df7d16112bc274f7f124963721
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-07-09 06:44:15 +0530

    Fix formatting in Two Sum solution and add Prettier configuration

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -10,8 +10,8 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0 ; i < nums.length ; i++)
         {
-            int sum = target - nums[i];
-            if(map.containsKey(sum))
+                                int sum = target - nums[i];
+                          if(map.containsKey(sum))
             {
                 return new int[]{map.get(sum),i};
             }
